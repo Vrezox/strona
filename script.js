394ce41b1f84fa88893f8f060f36999406a7eadb
Vrezox <62435433+Vrezox@users.noreply.github.com>
2025-06-12 18:57:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+});
+
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
